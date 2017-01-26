3b70b1aff2d65dbe6445da19ff6b90ec1fc83e0f
Andres Maza <amaza.ag@gmail.com>
2017-01-26 14:07:27 -0500

    Sorted winning logic, still need to figure out losing logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,49 @@
 $(document).ready(function() {
-  //alert("jQuery is working");
+
   var $submitInput = $("#user-submit");
   var $letterInput = $("input");
   var $hangmanStatus = $("#hangman");
   var $gameAnswer = $("#game-answer");
   var wordList = ["tree","dog","vehicle","bee","tomato","bread","random","hangman","awesome","sweet","telescope"];
+  var userInput = [];
+  var lifeCount = 4;
 
-  var newEntry = function() {
-    correctAnswer = wordList[Math.floor(Math.random() * wordList.length)];
-    var answerByLetter = correctAnswer.split("");
-    var newListItem = $("<li>");
+
+  var correctAnswer = wordList[Math.floor(Math.random() * wordList.length)];
+  var answerByLetter = correctAnswer.split("");
+
+  // Select a word at random from wordList
+  for(var i = 0; i < answerByLetter.length; i++) {
+    var $newListItem = $("<li>");
+    $("#game-answer ul").append($newListItem.html($("<p>").text(answerByLetter[i])));
+  }
+
+  //
+
+  var testFunc = function(){
+    var $userInputValue = $letterInput.val();
+    var $listItems = $("#game-answer ul li p");
 
     for(var i = 0; i < answerByLetter.length; i++) {
-      $("#game-answer ul").append(newListItem.text(answerByLetter[i]));
+      var $singleItem = $listItems.eq(i);
 
+      if($userInputValue === $singleItem.text()) {
+        $singleItem.attr("style","visibility: visible");
+        userInput.push($userInputValue);
+      } else if ($userInputValue !== $singleItem.text()){
+        console.log("Wrong one");
+      }
     }
-  };
+  }
 
-  newEntry();
+  $submitInput.on("click", function() {
+
+    if(userInput.length === answerByLetter.length){
+      alert("You've won!");
+    }
+    $letterInput.val("");
+  });
+
 
 
 });
