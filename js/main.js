db434567395c67c44b16b6ce5a50ce7299be544b
Andres Maza <amaza.ag@gmail.com>
2017-01-26 18:46:25 -0500

    Added some styling, fixed some bugs. Also added normalize.css

--- js/main.js
+++ js/main.js
@@ -9,58 +9,96 @@ $(document).ready(function() {
   var lifeCount = 6;
 
 
+
   var correctAnswer = wordList[Math.floor(Math.random() * wordList.length)];
   var answerByLetter = correctAnswer.split("");
 
-  // Select a word at random from wordList
-  for(var i = 0; i < answerByLetter.length; i++) {
-    var $newListItem = $("<li>");
-    $("#game-answer ul").append($newListItem.html($("<p>").text(answerByLetter[i])));
+    // Select a word at random from wordList
+  var startGame = function() {
+    for(var i = 0; i < answerByLetter.length; i++) {
+      var $newListItem = $("<li>");
+      $("#game-answer ul").append($newListItem.html($("<p>").text(answerByLetter[i])));
+    }
   }
-
-  //
 
   var gameLogic = function() {
     var $userInputValue = $letterInput.val();
     var $listItems = $("#game-answer ul li p");
 
+    if(userInput.indexOf($userInputValue, 0) !== -1) {
+      alert("You've selected that letter already!");
+      console.log(userInput);
+      return;
+    }
+
+
     for(var i = 0; i < answerByLetter.length; i++) {
       var $singleItem = $listItems.eq(i);
       var $singleItemText = $singleItem.text();
 
-      console.log($singleItemText);
-
-      if($userInputValue === $singleItemText) {
+      if ($userInputValue === $singleItemText) {
         $singleItem.attr("style","visibility: visible");
         userInput.push($userInputValue);
-      } else if (answerByLetter.indexOf($userInputValue, 0) === -1){
-        --lifeCount
-        console.log(lifeCount);
-        alert("Wrong One");
+      } else if (answerByLetter.indexOf($userInputValue, 0) === -1) {
+        --lifeCount;
         break;
       }
+
+      // if($userInputValue === $singleItemText) {
+      //   $singleItem.attr("style","visibility: visible");
+      //   userInput.push($userInputValue);
+      //   } else if (answerByLetter.indexOf($userInputValue, 0) === -1) {
+      //   --lifeCount
+      //   break;
+      // }
     }
+
     checkHangManState();
+
     if(userInput.length === answerByLetter.length) {
-      alert("You've won!");
-    } else if (lifeCount === 0){
-      alert("Aww, you've lost!");
+      setTimeout(function(){ alert("You've won!"); }, 150);
+      //startGame();
+      } else if (lifeCount === 0) {
+      setTimeout(function(){ alert("Aww man, you've lost!"); }, 150);
+      //startGame();
     }
     $letterInput.val("");
   }
 
-var checkHangManState = function () {
-  if(lifeCount === 5){
-    $("#hangman").css("background-color","red");
-  } else if(lifeCount === 4){
-    $("#hangman").css("background-color","blue");
-  } else if(lifeCount === 3){
-    $("#hangman").css("background-color","yellow");
+  var checkHangManState = function () {
+    switch(lifeCount) {
+      case 5:
+        $("#hangman").css("background-color","red");
+        break;
+      case 4:
+        $("#hangman").css("background-color","blue");
+        break;
+      case 3:
+        $("#hangman").css("background-color","yellow");
+        break;
+      case 2:
+        $("#hangman").css("background-color","black");
+        break;
+      case 1:
+        $("#hangman").css("background-color","green");
+        break;
+      case 0:
+        $("#hangman").css("background-color","gray");
+    }
   }
-}
-
-
 
   $submitInput.on("click", gameLogic);
 
+  $letterInput.on("keydown", function(event){
+    if(event.which === 13 && $letterInput.val() !== ""){
+      gameLogic();
+    }
   });
+
+  startGame();
+});
+
+
+//Make array of guesses
+//Check to see if user submitted letter matches one in array of guesses
+// if it does alert the user that the letter has already been submitted
