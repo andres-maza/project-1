947cbbb8203025b5ef7f882c48d48f204deb5811
Andres Maza <amaza.ag@gmail.com>
2017-01-27 16:35:36 -0500

    Fixed some small issues, added instructions modal

--- js/main.js
+++ js/main.js
@@ -26,9 +26,9 @@ $(document).ready(function() {
     var $listItems = $("#game-answer ul li p");
     // Check to see if the user's input is equal to an already guessed letter, if so alert "That letter has been selected"
     if(userInput.indexOf($userInputValue, 0) !== -1) {
-      alert("You've selected that letter already!");
-      $letterInput.val("");
-      return; // If condition above is met, exit function
+        alert("You've selected that letter already!");
+        $letterInput.val("");
+        return; // If condition above is met, exit function
     }
     // If user input matches any index of answerByLetter, set li element visiblity to visible.
     // Else substract 1 from lifeCount and break out of the loop
@@ -37,11 +37,14 @@ $(document).ready(function() {
       var $singleItemText = $singleItem.text();
 
       if ($userInputValue === $singleItemText) {
-        $singleItem.attr("style","visibility: visible");
-        userInput.push($userInputValue);
+          $singleItem.attr("style","visibility: visible");
+          userInput.push($userInputValue);
+      } else if ($userInputValue === "") {
+          alert("Ah! It doens't look like you entered any character. Please try again.");
+          break;
       } else if (answerByLetter.indexOf($userInputValue, 0) === -1) {
-        --lifeCount;
-        break;
+          --lifeCount;
+          break;
       }
     }
     // Check the value of lifeCount to and match it to case on switch statement below
@@ -50,15 +53,15 @@ $(document).ready(function() {
     // Else if lifeCount is equal to 0, user has lost the game
     // In either scenario, reload the window to start a new game
     if(userInput.length === answerByLetter.length) {
-      setTimeout(function() {alert("You've won!");});
-      lifeCount = 6;
-      userInput.length = 0;
-      setTimeout(function(){gameRestart();},300);
+        setTimeout(function() {alert("Congrats! You've won!");});
+        lifeCount = 6;
+        userInput.length = 0;
+        setTimeout(function(){gameRestart();},300);
       } else if (lifeCount === 0) {
-      setTimeout(function(){alert("Aww man, you've lost!");}, 150);
-      lifeCount = 6;
-      userInput.length = 0;
-      setTimeout(function(){gameRestart();},300);
+        setTimeout(function(){alert("Aww, you've lost!");}, 150);
+        lifeCount = 6;
+        userInput.length = 0;
+        setTimeout(function(){gameRestart();},300);
       }
     $letterInput.val("");
   }
@@ -93,7 +96,6 @@ $(document).ready(function() {
     (function(){
       var correctAnswer = wordList[Math.floor(Math.random() * wordList.length)];
       answerByLetter = correctAnswer.split("");
-
       for(var i = 0; i < answerByLetter.length; i++) {
         var $newListItem = $("<li>");
         $("#game-answer ul").append($newListItem.html($("<p>").text(answerByLetter[i])));
@@ -103,7 +105,34 @@ $(document).ready(function() {
     $("#hangman").css("background-image","url(assets/default.png)");
   }
 
+  var modalInstr = function() {
+    var $overlayBox = $("<div>", {id: "overlay"});
+    var $instrBox = $("<div>", {id: "instructions"});
+    var $closeBox = $("<button>", {id: "close-btn"}).text("x");
+    var $instrTxt = $("<p>", {id: "instr-txt"}).text(
+                                                `Select a letter of the alphabet
+                                                If the letter is contained in the word,
+                                                take another turn guessing a letter.
+                                                If the letter is not a part of the word,
+                                                a portion of the hangman is added.
+                                                Six incorrect letters and you've lost.
+                                                Guess all the correct letters to win!`
+                                              );
+
+    $("body").prepend($overlayBox);
+    $overlayBox.append($instrBox);
+    $instrBox.append($instrTxt);
+    $instrBox.append($closeBox);
+
+    $("#overlay").on("click", function closeOverlay() {
+      $("#overlay").fadeOut();
+    });
+
+    $("close-btn").on("click", closeOverlay());
+  }
+
   // On click and keydown event listeners
+  $("#instrBtn").on("click", modalInstr);
   $submitInput.on("click", gameLogic);
   $letterInput.on("keydown", function(event) {
     if(event.which === 13 && $letterInput.val() !== ""){
@@ -111,4 +140,5 @@ $(document).ready(function() {
     }
   });
   startGame();
+
 });
