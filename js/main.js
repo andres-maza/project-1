a78fde17b830f3092ad48e0cb2b5ca7d66ebcc6f
Andres Maza <amaza.ag@gmail.com>
2017-01-31 09:11:03 -0500

    Changes discussed with Arun

--- js/main.js
+++ js/main.js
@@ -4,19 +4,22 @@ $(document).ready(function() {
   var $letterInput = $("input");
   var $hangmanStatus = $("#hangman");
   var $gameAnswer = $("#game-answer");
+
   var wordList = ["tree","dog","vehicle","bee","tomato","bread","random","hangman","awesome","sweet","telescope", "seed", "premium", "harmony", "assembly", "momentous", "imperfect"];
+
   var userInput = [];
   var lifeCount = 6;
+  var answerByLetter;
   // Get random word from wordList and split into new array of answerByLetter
   var correctAnswer = wordList[Math.floor(Math.random() * wordList.length)];
+  //ARUN: Attach array to variable instead of window object
   answerByLetter = correctAnswer.split("");
 
   // Create new li element per value in answerByLetter and set its text equal to each index
   var startGame = function() {
-    for(var i = 0; i < answerByLetter.length; i++) {
-      var $newListItem = $("<li>");
-      $("#game-answer ul").append($newListItem.html($("<p>").text(answerByLetter[i])));
-    }
+    answerByLetter.forEach(function(item){
+      $("#game-answer ul").append($("<li>").append($("<p>").text(item)));
+    });
   }
 
   // Game functionality
@@ -26,9 +29,9 @@ $(document).ready(function() {
     var $listItems = $("#game-answer ul li p");
     // Check to see if the user's input is equal to an already guessed letter, if so alert "That letter has been selected"
     if(userInput.indexOf($userInputValue, 0) !== -1) {
-        alert("You've selected that letter already!");
-        $letterInput.val("");
-        return; // If condition above is met, exit function
+      alert("You've selected that letter already!");
+      $letterInput.val("");
+      return; // If condition above is met, exit function
     }
     // If user input matches any index of answerByLetter, set li element visiblity to visible.
     // Else substract 1 from lifeCount and break out of the loop
@@ -37,14 +40,14 @@ $(document).ready(function() {
       var $singleItemText = $singleItem.text();
 
       if ($userInputValue === $singleItemText) {
-          $singleItem.attr("style","visibility: visible");
-          userInput.push($userInputValue);
+        $singleItem.attr("style","visibility: visible");
+        userInput.push($userInputValue);
       } else if ($userInputValue === "") {
-          alert("Ah! It doens't look like you entered any character. Please try again.");
-          break;
+        alert("Ah! It doens't look like you entered any character. Please try again.");
+        break;
       } else if (answerByLetter.indexOf($userInputValue, 0) === -1) {
-          --lifeCount;
-          break;
+        --lifeCount;
+        break;
       }
     }
     // Check the value of lifeCount to and match it to case on switch statement below
@@ -53,16 +56,12 @@ $(document).ready(function() {
     // Else if lifeCount is equal to 0, user has lost the game
     // In either scenario, reload the window to start a new game
     if(userInput.length === answerByLetter.length) {
-        setTimeout(function() {alert("Congrats! You've won!");});
-        lifeCount = 6;
-        userInput.length = 0;
-        setTimeout(function(){gameRestart();},300);
-      } else if (lifeCount === 0) {
-        setTimeout(function(){alert("Aww, you've lost!");}, 150);
-        lifeCount = 6;
-        userInput.length = 0;
-        setTimeout(function(){gameRestart();},300);
-      }
+      setTimeout(function() {alert("Congrats! You've won!");});
+      setTimeout(function(){gameRestart();},300);
+    } else if (lifeCount === 0) {
+      setTimeout(function(){alert("Aww, you've lost!");}, 150);
+      setTimeout(function(){gameRestart();},300);
+    }
     $letterInput.val("");
   }
 
@@ -70,54 +69,56 @@ $(document).ready(function() {
   var checkHangManState = function () {
     switch(lifeCount) {
       case 5:
-        $("#hangman").css("background-image","url(assets/state-5.png)");
-        break;
+      $hangmanStatus.css("background-image","url(assets/state-5.png)");
+      break;
       case 4:
-        $("#hangman").css("background-image","url(assets/state-4.png)");
-        break;
+      $hangmanStatus.css("background-image","url(assets/state-4.png)");
+      break;
       case 3:
-        $("#hangman").css("background-image","url(assets/state-3.png)");
-        break;
+      $hangmanStatus.css("background-image","url(assets/state-3.png)");
+      break;
       case 2:
-        $("#hangman").css("background-image","url(assets/state-2.png)");
-        break;
+      $hangmanStatus.css("background-image","url(assets/state-2.png)");
+      break;
       case 1:
-        $("#hangman").css("background-image","url(assets/state-1.png)");
-        break;
+      $hangmanStatus.css("background-image","url(assets/state-1.png)");
+      break;
       case 0:
-        $("#hangman").css("background-image","url(assets/state-0.png)");
+      $hangmanStatus.css("background-image","url(assets/state-0.png)");
     }
   }
 
   // Logic to restart the game after win/lose
   var gameRestart = function(){
     $("li").detach();
-    delete answerByLetter;
-    (function(){
-      var correctAnswer = wordList[Math.floor(Math.random() * wordList.length)];
-      answerByLetter = correctAnswer.split("");
-      for(var i = 0; i < answerByLetter.length; i++) {
-        var $newListItem = $("<li>");
-        $("#game-answer ul").append($newListItem.html($("<p>").text(answerByLetter[i])));
-      }
-    })();
+    //Use undefined to clear out variable instead of delete
+    answerByLetter = [];
+    userInput = [];
+    lifeCount = 6;
+
+    var correctAnswer = wordList[Math.floor(Math.random() * wordList.length)];
+    answerByLetter = correctAnswer.split("");
+
+    startGame();
+
     $letterInput.val("");
-    $("#hangman").css("background-image","url(assets/default.png)");
+    $hangmanStatus.css("background-image","url(assets/default.png)");
   }
 
+  //ARUN: Better to put this in the HTML since the below HTML is not dynamic
   var modalInstr = function() {
     var $overlayBox = $("<div>", {id: "overlay"});
     var $instrBox = $("<div>", {id: "instructions"});
     var $closeBox = $("<button>", {id: "close-btn"}).text("x");
     var $instrTxt = $("<p>", {id: "instr-txt"}).text(
-                                                `Select a letter of the alphabet.
-                                                If the letter is contained in the word,
-                                                take another turn guessing a letter.
-                                                If the letter is not a part of the word,
-                                                a portion of the hangman is added.
-                                                Six incorrect letters and you've lost.
-                                                Guess all the correct letters to win!`
-                                              );
+      `Select a letter of the alphabet.
+      If the letter is contained in the word,
+      take another turn guessing a letter.
+      If the letter is not a part of the word,
+      a portion of the hangman is added.
+      Six incorrect letters and you've lost.
+      Guess all the correct letters to win!`
+    );
 
     $("body").prepend($overlayBox);
     $overlayBox.append($instrBox);
